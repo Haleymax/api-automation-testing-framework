dcd04e450dcf78696095da94f23687c2c568c022
Haley <huanghongweimax@163.com>
2025-04-14 14:26:24 +0800

    add report

--- testcases/test_api_demo/conftest.py
+++ testcases/test_api_demo/conftest.py
@@ -1,7 +1,18 @@
 import pytest
+import urllib3
+
+from config import setting
+from data.generate_case import generate_case
 
 
-@pytest.fixture
+
+@pytest.fixture(scope='module')
 def demo_api():
-    url = 'http://10.86.97.157:8000/api'
-    return url
+    url = 'http://10.86.97.157:8000/demo'
+    yield url
+    print("测试完成")
+
+
+
+
+api_case = generate_case(setting.YAML_FILE_PATH)
